343aa257246a64598b745f592cd4684b07f0b028
henriquewallace <henrique.wallace@hotmail.com>
2024-06-10 19:51:22 -0300

    first commit

---
+++ src/main/java/com/wallace/proposalapp/ProposalAppApplication.java
@@ -0,0 +1,13 @@
+package com.wallace.proposalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProposalAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wallace/proposalapp/ProposalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallace.proposalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
