075f57a2f9b973c621a1b6de4b12f5eb035faf30
henriquewallace <henrique.wallace@hotmail.com>
2024-06-11 00:39:36 -0300

    feat: use cascadeType as PERSIST to persist our user before persist the proposal

--- src/main/java/com/wallace/proposalapp/domain/Proposal.java
+++ src/main/java/com/wallace/proposalapp/domain/Proposal.java
@@ -1,12 +1,6 @@
 package com.wallace.proposalapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,7 +24,7 @@ public class Proposal {
     private boolean integrated;
     private String observation;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "id_user")
     private User user;
 }
