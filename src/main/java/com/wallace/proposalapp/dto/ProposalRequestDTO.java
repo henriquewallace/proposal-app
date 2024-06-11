2c0b5789a4dd1362b553b8422d6745c98b72a009
henriquewallace <henrique.wallace@hotmail.com>
2024-06-10 23:32:14 -0300

    refactor: update ProposalResponseDTO and create ProposalRequestDTO

---
+++ src/main/java/com/wallace/proposalapp/dto/ProposalRequestDTO.java
@@ -0,0 +1,21 @@
+package com.wallace.proposalapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProposalRequestDTO {
+
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+    private String cpf;
+    private Double renda;
+    private Double valorSolicitado;
+    private int prazoPagamento;
+}
--- src/main/java/com/wallace/proposalapp/dto/ProposalResponseDTO.java
+++ src/main/java/com/wallace/proposalapp/dto/ProposalResponseDTO.java
@@ -6,12 +6,16 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ProposalDTO {
+public class ProposalResponseDTO {
+
     private Long id;
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+    private String cpf;
+    private Double renda;
     private Double valorSolicitado;
     private int prazoPagamento;
     private Boolean aprovado;
-    private boolean integrado;
     private String observacao;
-    private User usuario;
 }
