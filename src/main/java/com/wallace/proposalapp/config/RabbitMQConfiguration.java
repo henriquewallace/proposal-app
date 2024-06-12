812e8fcf9bc84f7035c44af390f824da94008a31
henriquewallace <henrique.wallace@hotmail.com>
2024-06-11 22:49:26 -0300

    chore: create integration methods to initialize RabbitMQ and create the specified queues through spring

--- src/main/java/com/wallace/proposalapp/config/RabbitMQConfiguration.java
+++ src/main/java/com/wallace/proposalapp/config/RabbitMQConfiguration.java
@@ -2,6 +2,10 @@ package com.wallace.proposalapp.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,5 +30,15 @@ public class RabbitMQConfiguration {
     @Bean
     public Queue createFinishedProposalNotificationQueue() {
         return QueueBuilder.durable("finished-proposal.notification-service").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initializerAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
     }
 }
