b282b7f85f450ae073d1263ace80d8f53b37073c
henriquewallace <henrique.wallace@hotmail.com>
2024-06-12 22:57:15 -0300

    create native query in repository to get all proposals where integrated is false

--- src/main/java/com/wallace/proposalapp/repository/ProposalRepository.java
+++ src/main/java/com/wallace/proposalapp/repository/ProposalRepository.java
@@ -1,9 +1,16 @@
 package com.wallace.proposalapp.repository;
 
 import com.wallace.proposalapp.domain.Proposal;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProposalRepository extends CrudRepository<Proposal, Long> {
+
+    @Query(value = "select * from tb_proposal as p" +
+            " where p.integrated = false", nativeQuery = true)
+    List<Proposal> getAllProposalsByIntegrated();
 }
